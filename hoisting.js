14d8082679e68c9d58bf1bc4a0c66f22987ab4f0
Marvelles Fisher <mfisher91@live.com>
2021-06-09 08:44:40 -0700

    AdvancedJava

---
+++ hoisting.js
@@ -0,0 +1,36 @@
+setName();
+
+function setName() {
+    var name = 'Marvelles';
+    console.log(name);
+}
+console.log('some text');
+let avg = findAvg(2, 2);
+console.log('some text', avg);
+
+function findAvg(a, b) {
+    console.log('some text');
+    var answer = (a + b) / 2;
+    return answer;
+}
+let fruits = ['apple', 'tomato', 'banana'];
+let favFruit;
+
+function printFruits() {
+    let favFruit = fruits[2];
+    console.log(fruits[0]);
+}
+
+function printFavFruit() {
+    console.log(leastFavFruit)
+    console.log(favFruit);
+    let leastFavFrui = ['pineapple', 'strawberry', 'grapes'];
+}
+console.log(Hello);
+name = Marvelles
+let leastFavFruit =
+
+
+    function Hello() {}
+printFruits();
+//printFavFruit();//can no longer call this here
